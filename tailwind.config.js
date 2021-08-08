81be1dde7f5898d057fb1c16ac0dec2cfba3ec9a
Jon Meyers <jonathon.d.meyers@gmail.com>
2021-08-08 22:31:00 +1000

    remove taildwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  purge: ["./pages/**/*.{js}", "./components/**/*.{js}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
